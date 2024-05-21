bf8bd4eb40d848eff37b4befec16193232179082
biabbiassago <bbrusco@uci.edu>
2024-05-21 11:07:35 -0700

    gitignore again

--- src/run-models/exploratory/test-pref-median-base.R
+++ src/run-models/exploratory/test-pref-median-base.R
@@ -3,8 +3,8 @@
 library(rstan)
 source(here::here("src/run-models/run-funcs.R"))
 
-fd <- readRDS(here::here("data/test-median-pref/prefonmedian-0.898.rds"))
-stan_data <- prepare_stan_data_no_pref(fd)
+dat_sample <- readRDS(here::here("data/test-median-pref/prefonmedian-0.898.rds"))
+stan_data <- prepare_stan_data_no_pref(dat_sample)
 random_data_id <- 0.898
 
 CODE_PATH <- here::here("src/stan-files/fit-repgev.stan")
@@ -19,8 +19,9 @@ stan_fit_median_no_pref <- stan(
   init = init_vals,
   warmup = 2000,
   pars = c("SIGMAETA","SIGMANU","SIGMAPHI","L_SE","L_SN","L_SPHI"),
-  include=FALSE
-  #control= list(max_treedepth = 12)
+  include=FALSE,
+  control= list(max_treedepth = 12)
 )
 
-saveRDS(stan_fit_median_no_pref,here::here(paste0("outputs/test-pref-on-median-0524/modelbaseiters5k-",random_data_id,".rds")))
+saveRDS(
+  stan_fit_median_no_pref,here::here(paste0("outputs/test-pref-on-median-0524/modelbaseiters5k-",random_data_id,".rds")))
